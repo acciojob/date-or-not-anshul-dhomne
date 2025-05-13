9b7b67a97c4ad0bf3ebba83a7b2da7cc51afc1c7
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-05-13 15:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var isDate = function (input) {
-  //   write your code here
-	    return !isNAN(Date.parse(input));
+    // Check if input is a Date object and if it's valid
+    if (input instanceof Date && !isNaN(input.getTime())) {
+        return true;
+    }
+    
+    // Check if input is a string and can be parsed into a valid date
+    if (typeof input === 'string') {
+        return !isNaN(Date.parse(input));
+    }
+    
+    // For any other type of input, return false
+    return false;
 };
 
 // Do not change the code below.
